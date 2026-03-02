91ab4dd94b36e8a8d05a7cfc9b02d574ae1fa91d
Frank Hildebrandt <frank@hildebrandt.io>
2026-03-02 15:59:31 +0100

    Fix text input focus on app launch

--- Sources/CaddyApp/Services/AppWindowController.swift
+++ Sources/CaddyApp/Services/AppWindowController.swift
@@ -26,6 +26,11 @@ final class MainWindowCloseDelegate: NSObject, NSWindowDelegate {
 }
 
 final class AppLifecycleDelegate: NSObject, NSApplicationDelegate {
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        NSApp.setActivationPolicy(.regular)
+        NSApp.activate(ignoringOtherApps: true)
+    }
+
     func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
         false
     }
